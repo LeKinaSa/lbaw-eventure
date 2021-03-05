1e2a67bc0b842d22719beb55768d68ccc6d32bef
Gonçalo Pascoal <up201806332@fe.up.pt>
2021-03-05 17:57:49 +0000

    Added initial FAQ page

---
+++ html/pages/faq.php
@@ -0,0 +1,57 @@
+<? include_once('../templates/header.php'); ?>
+
+<div class="container p-3">
+    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="homepage.php">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">FAQ</li>
+        </ol>
+    </nav>
+
+    <section id="frequentlyAskedQuestions" class="container">
+        <h1 class="text-center">Frequently Asked Questions</h1>
+        <div class="accordion" id="faqAccordion">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="heading1">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
+                        How do I join an event?
+                    </button>
+                </h2>
+                <div id="collapse1" class="accordion-collapse collapse" aria-labelledby="heading1" data-bs-parent="#faqAccordion">
+                    <div class="accordion-body">
+                        To join an event, you first need to create an account on the website. Once you are authenticated, 
+                        after navigating to the page of the event you want to join, click the request to participate button.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="heading2">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse1">
+                        How do I invite someone do my private event?
+                    </button>
+                </h2>
+                <div id="collapse2" class="accordion-collapse collapse" aria-labelledby="heading2" data-bs-parent="#faqAccordion">
+                    <div class="accordion-body">
+                        To invite someone to your private event, click the invitations link in the event's page. Afterwards, you can 
+                        invite them by entering their username or email.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="heading3">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse1">
+                        How do I create a poll?
+                    </button>
+                </h2>
+                <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading3" data-bs-parent="#faqAccordion">
+                    <div class="accordion-body">
+                        Navigate to your event's page.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+<? include_once('../templates/footer.php'); ?>
