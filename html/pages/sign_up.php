6403cb02ffa6f9bb58a32717f1dd7580f750ed23
Clara Martins <50105043+CAMartins@users.noreply.github.com>
2021-03-06 14:42:32 +0000

    Sign Up Page

---
+++ html/pages/sign_up.php
@@ -0,0 +1,54 @@
+<?php
+    include_once('../templates/header.php');
+?>
+<div class="container-fluid p-3">
+    <div class="align-self-center text-center">
+        <h1 class="display-4">Create your Account</h1>
+        <div class="d-grid gap-4">
+            <div class="row justify-content-center">
+                <div class="col-3">
+                    <form>
+                        <div class="mb-3 text-start">
+                            <label for="username" class="form-label">
+                                Username *
+                            </label>
+                            <input type="text" class="form-control" id="username">
+                            
+                            <label for="email" class="form-label">
+                                Email address *
+                            </label>
+                            <input type="email" class="form-control" id="email">
+                            
+                            <label for="password" class="form-label">
+                                Password *
+                            </label>
+                            <input type="password" class="form-control" id="password">
+
+                            <label for="passwordConfirm" class="form-label">
+                                Confirm your Password *
+                            </label>
+                            <input type="password" class="form-control" id="passwordConfirm">
+                        </div>
+                        <div class="d-grid col-12">
+                            <button type="submit" class="btn btn-primary btn-block">
+                                Create Account
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-3">
+                    <div class="d-grid col-12">
+                        <button type="button" class="btn btn-outline-primary btn-block">
+                            Sign up with your <img src="../assets/google.png" class="google" alt="Google"> Account
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    include_once('../templates/footer.php');
+?>
