31bcc05dc59e56ecf37360faebbfdc305e76c6fa
Gonçalo Pascoal <up201806332@fe.up.pt>
2021-03-07 17:29:21 +0000

    Code cleanup and tweaks to homepage

--- html/pages/homepage.php
+++ html/pages/homepage.php
@@ -1,11 +1,13 @@
 <?php
     include_once('../templates/header.php');
 ?>
-<div class="row">
-    <div class="col-8">
+
+<div class="container-fluid p-0">
+    <div class="col-md-8">
         <img src="../assets/chess.jpg" class="img-fluid" alt="An image">
     </div>
 </div>
+
 <?php
     include_once('../templates/footer.php');
 ?>
