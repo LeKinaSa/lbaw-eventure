9e6abb16cd461b522fbff9e1379d45165076dc26
Clara Martins <50105043+CAMartins@users.noreply.github.com>
2021-03-08 22:26:04 +0000

    Homepage Improvements

--- html/pages/homepage.php
+++ html/pages/homepage.php
@@ -3,8 +3,61 @@
 ?>
 
 <div class="container-fluid p-0">
-    <div class="col-md-8">
-        <img src="../assets/chess.jpg" class="img-fluid" alt="An image">
+    <div class="row">
+        <div class="col-md-8">
+            <img src="../assets/chess.jpg" class="img-fluid" alt="An image">
+            <h1 class="display-4 text-center p-3">
+                Helping you organize and participate in all the events you dream of
+            </h1>
+        </div>
+        <div class="col-md-4 text-center p-3">
+            <h5>Top Events this Week</h5>
+            <div class="card mt-2">
+                <div class="card-body">
+                    <h5 class="card-title"><a href="event.php">Magic: the Gathering Tournament</a></h5>
+                    <div class="row align-items-center h-100">
+                        <div class="col-md-6">
+                            <img class="card-img-bottom" src="../assets/dragons.jpg" alt="Magic: the Gathering Tournament"> 
+                        </div>
+                        <div class="col-md-6">
+                            <p class="card-text">
+                                27/02/2021<br>Rua da Vida, nº33<br>134 participants
+                            </p>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-2">
+                <div class="card-body">
+                    <h5 class="card-title"><a href="event.php">Chess: Champions Arisen</a></h5>
+                    <div class="row align-items-center h-100">
+                        <div class="col-md-6">
+                            <img class="card-img-bottom" src="../assets/chess_event.jpg" alt="Magic: the Gathering Tournament"> 
+                        </div>
+                        <div class="col-md-6">
+                            <p class="card-text">
+                                01/03/2021<br>Rua das Tulipas, nº154<br>122 participants
+                            </p>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-2">
+                <div class="card-body">
+                    <h5 class="card-title"><a href="event.php">Starcraft 2 Invitational</a></h5>
+                    <div class="row align-items-center h-100">
+                        <div class="col-md-6">
+                            <img class="card-img-bottom" src="../assets/starcraft2.jpg" alt="Magic: the Gathering Tournament"> 
+                        </div>
+                        <div class="col-md-6">
+                            <p class="card-text">
+                                04/03/2021<br>Rua do Castelo, nº1<br>152 participants
+                            </p>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
