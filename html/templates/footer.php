78e0ddb8f313acdde4e5f0a8ae5432d11475dd51
Clara Martins <50105043+CAMartins@users.noreply.github.com>
2021-03-08 18:39:20 +0000

    Revert Footer Changes

--- html/templates/footer.php
+++ html/templates/footer.php
@@ -4,14 +4,12 @@
                 <div class="col-md-6 mb-2">
                     <span>© Copyright 2021 Eventure</span>
                 </div>
-                <div class="col-md-1">
+                <div class="col-md-6">
                     <h5 class="text-uppercase">Help</h5>
-                </div>
-                <div class="col-md-5">
-                    <ul class="list-unstyled list-inline">
-                        <li class="list-inline-item"><a href="about.php">About</a></li>
-                        <li class="list-inline-item"><a href="contacts.php">Contacts</a></li>
-                        <li class="list-inline-item"><a href="faq.php">FAQ</a></li>
+                    <ul class="list-unstyled">
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="contacts.php">Contacts</a></li>
+                        <li><a href="faq.php">FAQ</a></li>
                     </ul>
                 </div>
             </div>
