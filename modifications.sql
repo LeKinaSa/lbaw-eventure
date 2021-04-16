e56cde4d6d0bef86cc4b7ce1e78fbd19978fb447
afonsocaiado <afonsocaiado1@gmail.com>
2021-04-16 12:19:47 +0100

    all current inserts done except 1, not tested

--- modifications.sql
+++ modifications.sql
@@ -12,24 +12,36 @@ INSERT INTO comment (id, id_author, id_event, id_parent, "text", "date")
 -- INSERT03 - Answer a poll
 
 INSERT INTO poll_answer (id_user, id_poll, id_poll_option)
-    VALUES ($id_user, $id_poll, $id_poll_option)
+    VALUES ($id_user, $id_poll, $id_poll_option);
 
--- INSERT04 - Send invitations
+-- INSERT04 - Create polls
 
--- INSERT05 - Post results
+INSERT INTO poll (id, id_event, question)
+    VALUES ($id, $id_event, $question);
 
--- INSERT06 - Create polls
+-- INSERT05 - Upload files
 
--- INSERT07 - Upload files
+INSERT INTO "file" (id, id_event, "name", "data", date_uploaded)
+    VALUES ($id, $id_event, $file_name, $file_data, $date_uploaded);
 
--- INSERT08 - Suspend users
+-- INSERT06 - Suspend users
 
--- INSERT09 - Ban users
+INSERT INTO suspension (id, id_user, "from", until, reason)
+    VALUES ($id, $id_user, $from_date, $until, $reason);
+
+-- INSERT07 - Ban users
+
+INSERT INTO banned_user (id_user, since, reason)
+    VALUES ($id_user, $since, $reason);
+
+-- INSERT08 - Send invitations ??
 
 -- UPDATE01 - Update personal information
 
 -- UPDATE02 - Manage invitations
 
 -- UPDATE03 - Manage event details
+
+-- UPDATE04 - Post results
 
 -- DELETE01 - Delete account
