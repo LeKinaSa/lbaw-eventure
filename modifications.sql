d4f12cf501e74c69cb68e692234913a569ba4ab5
afonsocaiado <afonsocaiado1@gmail.com>
2021-04-16 11:58:19 +0100

    Added new file for updates/delete/inserts

---
+++ modifications.sql
@@ -0,0 +1,26 @@
+
+-- INSERT01 - Create an event
+
+-- INSERT02 - Comment on an event
+
+-- INSERT03 - Answer a poll
+
+-- INSERT04 - Send invitations
+
+-- INSERT05 - Post results
+
+-- INSERT06 - Create polls
+
+-- INSERT07 - Upload files
+
+-- INSERT08 - Suspend users
+
+-- INSERT09 - Ban users
+
+-- UPDATE01 - Update personal information
+
+-- UPDATE02 - Manage invitations
+
+-- UPDATE03 - Manage event details
+
+-- DELETE01 - Delete account
