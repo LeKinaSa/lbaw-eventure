86390248cf3a57c7e7ba32350292e8f74757fcc7
afonsocaiado <afonsocaiado1@gmail.com>
2021-04-16 12:10:11 +0100

    started inserts

--- modifications.sql
+++ modifications.sql
@@ -1,9 +1,18 @@
 
 -- INSERT01 - Create an event
 
+INSERT INTO "event" (id, id_organizer, title, visibility, "description", keywords, picture, "start_date", end_date, "type", "location", max_attendance, cancelled, id_category, win_points, draw_points, loss_points, leaderboard)
+    VALUES ($id, $id_organizer, $title, $visibility, $event_description, $keywords, $picture, $starting_date, $end_date, $event_type, $event_location, $max_attendance, $cancelled, $id_category, $win_points, $draw_points, $loss_points, $leaderboard);
+
 -- INSERT02 - Comment on an event
 
+INSERT INTO comment (id, id_author, id_event, id_parent, "text", "date")
+    VALUES ($id, $id_author, $id_event, $id_parent, $comment_text, $comment_date);
+
 -- INSERT03 - Answer a poll
+
+INSERT INTO poll_answer (id_user, id_poll, id_poll_option)
+    VALUES ($id_user, $id_poll, $id_poll_option)
 
 -- INSERT04 - Send invitations
 
