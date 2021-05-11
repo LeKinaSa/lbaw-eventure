e3ed774a92f4e4a47a6c6a723981f9687edf276d
afonsocaiado <afonsocaiado1@gmail.com>
2021-05-11 17:39:13 +0100

    Base partial match created

---
+++ resources/views/partials/match.blade.php
@@ -0,0 +1,7 @@
+<div class="card" style="max-width: 400px">
+    <div class="card-body">
+        <h5 class="card-title"><span class="text-primary">Dmitri Dolyakov</span> vs <span class="text-primary">Martin Fowler</span></h5>
+        <p class="card-text"><b>Result:</b> Win (Dmitri Dolyakov)</p>
+        <p class="card-text text-muted">Dolyakov wins with the white pieces after Fowler resigns</p>
+    </div>
+</div>
