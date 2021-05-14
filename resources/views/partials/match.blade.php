8b769e11ecba45ef1503233b6aff78ba1b54e353
afonsocaiado <afonsocaiado1@gmail.com>
2021-05-14 20:08:36 +0100

    Updated match partial blade, complete?

--- resources/views/partials/match.blade.php
+++ resources/views/partials/match.blade.php
@@ -1,7 +1,7 @@
 <div class="card" style="max-width: 400px">
     <div class="card-body">
-        <h5 class="card-title"><span class="text-primary">Dmitri Dolyakov</span> vs <span class="text-primary">Martin Fowler</span></h5>
-        <p class="card-text"><b>Result:</b> Win (Dmitri Dolyakov)</p>
-        <p class="card-text text-muted">Dolyakov wins with the white pieces after Fowler resigns</p>
+        <h5 class="card-title"><span class="text-primary">{{ $competitor1->name }}</span> vs <span class="text-primary">{{ $competitor2->name }}</span></h5>
+        <p class="card-text"><b>Result:</b>{{ $match->result }}</p>
+        <p class="card-text text-muted">{{ $match->description }}</p>
     </div>
 </div>
