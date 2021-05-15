b718e8853cc80a10651221aef523412a4df0699d
afonsocaiado <afonsocaiado1@gmail.com>
2021-05-15 14:45:34 +0100

    Yet to replace competitor's id with names

--- resources/views/partials/match.blade.php
+++ resources/views/partials/match.blade.php
@@ -1,6 +1,6 @@
 <div class="card" style="max-width: 400px">
     <div class="card-body">
-        <h5 class="card-title"><span class="text-primary">{{ $competitor1->name }}</span> vs <span class="text-primary">{{ $competitor2->name }}</span></h5>
+        <h5 class="card-title"><span class="text-primary">{{ $match->id_competitor1 }}</span> vs <span class="text-primary">{{ $match->id_competitor2 }}</span></h5>
         <p class="card-text"><b>Result:</b>{{ $match->result }}</p>
         <p class="card-text text-muted">{{ $match->description }}</p>
     </div>
