cfe733d791c4ba40bb501330b8d5e96c7b477e56
Clara Martins <50105043+LeKinaSa@users.noreply.github.com>
2021-05-27 09:58:10 +0100

    Password Reset should be working now
    Email is not sending, i dont know why

---
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+
+class ResetPasswordController extends Controller {
+    protected $redirectTo = '/';
+    use ResetsPasswords;
+
+    /**
+     * Show the reset password page.
+     * This page should contain a form containing the following inputs:
+     * email, password, password_confirmation and hidden token
+     * 
+     * @param String $token - the email token (64 chars)
+     * @return \Illuminate\Http\Response
+     */
+    public function show($token) {
+        // TODO: authorization
+        return view('auth.recover_password', ['token' => $token]);
+    }
+
+    /**
+     * Reset the password of the account with the email and token of the request.
+     * 
+     * @param Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function recoverPassword(Request $request) {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) use ($request) {
+                $user->update([
+                    'password' => bcrypt($password)
+                ]);
+    
+                $user->save();
+    
+                event(new PasswordReset($user));
+            }
+        );
+
+        if ($status === Password::PASSWORD_RESET) {
+            // TODO
+            return back();
+        }
+
+        return back()->withErrors(['token' => [__($status)]]);
+    }
+}
