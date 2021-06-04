aacf88aa237549e884862ee51b77f26f4a09566c
Gonçalo Pascoal <up201806332@fe.up.pt>
2021-06-04 11:50:45 +0100

    Improved layout of delete file button

--- resources/views/partials/file.blade.php
+++ resources/views/partials/file.blade.php
@@ -4,15 +4,15 @@
             <h5 class="card-title">{{ $file->name }}</h5>
             <p class="card-subtitle text-muted">Uploaded {{ (new DateTime($file->date_uploaded))->format('j M, Y H:i') }}</p>
         </div>
-        <a role="button" class="btn btn-primary" href="{{ route('events.event.files.file', ['id' => $event->id, 'fileName' => $file->name]) }}"><i class="fa fa-download"></i></a>
+        <div class="d-flex flex-column align-items-center gap-1">
+            <a role="button" class="btn btn-primary" href="{{ route('events.event.files.file', ['id' => $event->id, 'fileName' => $file->name]) }}"><i class="fa fa-download"></i></a>
+            @if (App\Policies\FilePolicy::delete(Auth::user() ?? Auth::guard('admin')->user(), $file))
+            <form method="POST" action="{{ route('events.event.files.delete', ['id' => $event->id, 'fileId' => $file->id]) }}" class="form-delete-file">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger" aria-label="Delete File"><i class="fa fa-remove"></i></button>
+            </form>
+            @endif
+        </div>
     </div>
-    @if (App\Policies\FilePolicy::delete(Auth::user() ?? Auth::guard('admin')->user(), $file))
-    <div class="card-footer text-center">
-        <form method="POST" action="{{ route('events.event.files.delete', ['id' => $event->id, 'fileId' => $file->id]) }}" class="form-delete-file">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger" aria-label="Delete File">Delete this file</button>
-        </form>
-    </div>
-    @endif
 </article>
